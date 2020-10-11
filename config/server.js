8c1bb53fb97d2d726c9aa389e569326dbdaf0c42
Juan Ovalle <juanovalle9@gmail.com>
2020-10-11 19:35:51 -0400

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0c864591bf34f1173715394506d2afe4'),
+    },
+  },
+});
